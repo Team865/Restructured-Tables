d0764e54e817b4d216a4a74de008f2a9c52edd2f
yuliu2016 <22801773+yuliu2016@users.noreply.github.com>
2018-11-28 09:35:30 -0500

    add event config

--- src/main/java/ca/warp7/tables/controller/AppBarController.java
+++ src/main/java/ca/warp7/tables/controller/AppBarController.java
@@ -2,30 +2,41 @@ package ca.warp7.tables.controller;
 
 import ca.warp7.tables.view.WebCamWindow;
 import javafx.fxml.FXML;
-import javafx.scene.control.Alert;
-import javafx.scene.control.ButtonType;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.FileChooser;
 import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class AppBarController {
 
     @FXML
     void onEventSettingsAction() {
-        Alert alert = new Alert(Alert.AlertType.CONFIRMATION, "Are you sure?",
-                ButtonType.YES, ButtonType.NO, ButtonType.CANCEL);
-        alert.showAndWait();
-        System.out.println("Hi");
+        try {
+            Stage stage = new Stage();
+            FXMLLoader loader = new FXMLLoader();
+            loader.setLocation(getClass().getResource("/stages/eventconfig/Main.fxml"));
+            stage.setTitle("Event Configuration");
+            stage.getIcons().add(new Image(getClass().getResourceAsStream("/app-icon.png")));
+            stage.setScene(new Scene(loader.load()));
+            stage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     private WebCamWindow webCamWindow = new WebCamWindow();
 
     @FXML
-    void onScanNowAction(){
+    void onScanNowAction() {
         webCamWindow.start();
     }
 
     @FXML
-    void onImportDataAction(){
+    void onImportDataAction() {
         FileChooser fileChooser = new FileChooser();
         fileChooser.setTitle("Open Resource File");
         fileChooser.showOpenDialog(null);
