b9783936d655c0ae293d12565d32c817f5feb665
yuliu2016 <22801773+yuliu2016@users.noreply.github.com>
2018-11-27 21:18:45 -0500

    modularize fxml

--- src/main/java/ca/warp7/tables/TablesApp.java
+++ src/main/java/ca/warp7/tables/TablesApp.java
@@ -5,7 +5,6 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.image.Image;
-import javafx.scene.layout.HBox;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -17,7 +16,7 @@ public class TablesApp extends Application {
         primaryStage.getIcons().add(new Image(getClass().getResourceAsStream("/app-icon.png")));
         FXMLLoader loader = new FXMLLoader();
 
-        loader.setLocation(getClass().getResource("/fxml/main.fxml"));
+        loader.setLocation(getClass().getResource("/fxml/Main.fxml"));
 
         try {
             Parent hBox = loader.load();
