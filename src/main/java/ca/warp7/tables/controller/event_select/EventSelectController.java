4fb38e0c1f0e5151f6fcc26d3c5a281bf8c9a289
yuliu2016 <22801773+yuliu2016@users.noreply.github.com>
2018-11-28 22:26:58 -0500

    add event select controller

---
+++ src/main/java/ca/warp7/tables/controller/event_select/EventSelectController.java
@@ -0,0 +1,22 @@
+package ca.warp7.tables.controller.event_select;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
+import javafx.scene.control.ButtonType;
+import javafx.scene.control.TextInputDialog;
+
+public class EventSelectController {
+    @FXML
+    void onDeleteAction(){
+        Alert alert = new Alert(Alert.AlertType.WARNING,
+                "Delete Event 2018iri? There is no going back!",
+                ButtonType.YES, ButtonType.NO);
+        alert.showAndWait();
+    }
+
+    @FXML
+    void onBlueAllianceEventAction(){
+        TextInputDialog dialog = new TextInputDialog("Enter The Blue Alliance key");
+        dialog.showAndWait();
+    }
+}
