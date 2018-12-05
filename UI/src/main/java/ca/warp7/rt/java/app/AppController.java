a009c1e73a5ae63df4f545785c30a2770684e338
yuliu2016 <22801773+yuliu2016@users.noreply.github.com>
2018-12-05 09:26:36 -0500

    merge AppBar into App controller

--- UI/src/main/java/ca/warp7/rt/java/app/AppController.java
+++ UI/src/main/java/ca/warp7/rt/java/app/AppController.java
@@ -1,10 +1,15 @@
 package ca.warp7.rt.java.app;
 
 import ca.warp7.rt.java.base.StageController;
+import ca.warp7.rt.java.base.StageUtils;
 import javafx.fxml.FXML;
+import javafx.scene.control.MenuButton;
+import javafx.scene.control.MenuItem;
 import javafx.scene.input.KeyCode;
 import javafx.scene.input.KeyCodeCombination;
 import javafx.stage.Stage;
+
+import static ca.warp7.rt.java.base.StageUtils.stage;
 
 public class AppController implements StageController {
 
@@ -19,6 +24,35 @@ public class AppController implements StageController {
     }
 
     @FXML
+    MenuButton newButton;
+
+    @FXML
+    void onEventSelectAction() {
+        stage("/ca/warp7/rt/stage/app/EventSelect.fxml", "Select Event", getClass());
+    }
+
+    @FXML
+    void onSystemStateAction() {
+        stage("/ca/warp7/rt/stage/app/SystemState.fxml", "System State", getClass());
+    }
+
+    @FXML
+    void onScannerAction() {
+        stage("/ca/warp7/rt/stage/scanner/Scanner.fxml", "Scanner", getClass());
+    }
+
+    @FXML
+    void onPythonScripts() {
+        stage("/ca/warp7/rt/stage/python/PythonScripts.fxml", "Python Scripts", getClass());
+    }
+
+    @FXML
     void initialize() {
+        Features.multiTabFeatures.forEach(multiTab -> {
+            MenuItem item = new MenuItem();
+            item.setText(multiTab.getFeatureName());
+            item.setGraphic(StageUtils.icon(multiTab.getIconLiteral()));
+            newButton.getItems().add(item);
+        });
     }
 }
