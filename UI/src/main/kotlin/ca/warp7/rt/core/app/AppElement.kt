a8fe690173df09d88e55d8b54f585e11e97896bc
yuliu2016 <22801773+yuliu2016@users.noreply.github.com>
2018-12-24 08:06:55 -0500

    clean up java #1

--- UI/src/main/kotlin/ca/warp7/rt/core/app/AppElement.kt
+++ UI/src/main/kotlin/ca/warp7/rt/core/app/AppElement.kt
@@ -2,8 +2,6 @@ package ca.warp7.rt.core.app
 
 import ca.warp7.rt.core.env.EnvUser
 import ca.warp7.rt.core.env.EnvUtils
-import ca.warp7.rt.core.feature.FeatureIcon
-import ca.warp7.rt.core.feature.FeatureItemTab
 import ca.warp7.rt.core.feature.FeatureLink
 import javafx.geometry.Pos
 import javafx.scene.control.Label
@@ -14,24 +12,6 @@ internal object AppElement {
 
     @JvmStatic
     val teamColor: Color = Color.valueOf("1e2e4a")
-
-    @JvmStatic
-    fun tabUIFromAction(tab: AppTab): HBox {
-        val action = tab.featureItemTab
-        val outer = HBox()
-        val inner = HBox()
-        inner.prefWidth = 24.0
-        inner.alignment = Pos.CENTER
-        val icon = FeatureIcon(action.iconLiteral)
-        icon.iconColor = teamColor
-        inner.children.add(icon)
-        tab.icon = icon
-        outer.alignment = Pos.CENTER_LEFT
-        outer.spacing = 10.0
-        outer.children.add(inner)
-        outer.children.add(Label(action.title))
-        return outer
-    }
 
     fun tabUIFromLink(link: FeatureLink): HBox {
         val outer = HBox()
@@ -47,19 +27,6 @@ internal object AppElement {
         return outer
     }
 
-
-    @JvmStatic
-    fun getTitle(tab: FeatureItemTab): String {
-        val title: String
-        if (tab.tabGroup == FeatureItemTab.Group.SingleTab)
-            title = String.format("%s", tab.title)
-        else {
-            val id = tab.featureId
-            val capId = id.substring(0, 1).toUpperCase() + id.substring(1)
-            title = String.format("%s - %s", tab.title, capId)
-        }
-        return title
-    }
 
     @JvmStatic
     fun getUserExplicitName(): String {
