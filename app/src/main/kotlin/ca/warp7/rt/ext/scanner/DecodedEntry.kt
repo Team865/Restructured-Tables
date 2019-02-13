65198a9edd04b247db6078636da1e6f1c41eb810
yuliu2016 <22801773+yuliu2016@users.noreply.github.com>
2019-02-13 08:35:11 -0500

    fix decoding algorithm

--- app/src/main/kotlin/ca/warp7/rt/ext/scanner/DecodedEntry.kt
+++ app/src/main/kotlin/ca/warp7/rt/ext/scanner/DecodedEntry.kt
@@ -15,11 +15,11 @@ data class DecodedEntry(override val encoded: String) : V5Entry {
     override val timestamp = split[4].toInt(16)
     override val undone = split[5].toInt()
     override val comments = split[7]
-    override val dataPoints = DatatypeConverter.parseBase64Binary(split[6])
-            .run { (0 until size / 3).map { DataPoint(get(it).toInt(), get(it + 1).toInt(), get(it + 2).toInt()) } }
+    override val dataPoints = DatatypeConverter.parseBase64Binary(split[6]).map { it.toInt() }
+            .run { (0 until size / 3).map { DataPoint(get(it * 3), get(it * 3 + 1), get(it * 3 + 2)) } }
     private val length = dataPoints.size
 
     override fun count(type: Int) = dataPoints.subList(0, length).filter { it.type == type }.size
     override fun lastValue(type: Int) = dataPoints.subList(0, length).lastOrNull { it.type == type }
-    override fun focused(type: Int) = false
+    override fun focused(type: Int, time: Int) = dataPoints.any { it.type == type && it.time == time }
 }
