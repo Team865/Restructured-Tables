d264d10207697c2a0cc80522bd2384c547d8d69e
yuliu2016 <22801773+yuliu2016@users.noreply.github.com>
2019-03-06 22:27:58 -0500

    add column headers

--- app/src/main/kotlin/ca/warp7/rt/ext/humber/Humber.kt
+++ app/src/main/kotlin/ca/warp7/rt/ext/humber/Humber.kt
@@ -51,6 +51,7 @@ object Endgame {
 val startPositions = listOf("None", "L2", "L1", "C1", "R1", "R2")
 val climbLevels = listOf("None", "1", "2", "3")
 val liftingLevels = listOf("None", "2", "3")
+val gamePieces = listOf("Cargo", "None", "Hatch")
 
 val longFormatter = SimpleDateFormat("yyyy-dd-MM HH:mm:ss")
 
@@ -58,21 +59,53 @@ val longFormatter = SimpleDateFormat("yyyy-dd-MM HH:mm:ss")
 fun process(data: List<V5Entry>): DataFrame {
     val rows = mutableListOf<Map<String, Any>>()
     for (entry in data) {
+        var gamePiece = 0
+        var opponentField = 0
+        var defended = 0
+
         rows.add(mapOf(
                 "Match" to entry.match,
                 "Team" to entry.team,
                 "Alliance" to entry.board.alliance.toString(),
                 "Scout" to entry.scout,
                 "Board" to entry.board.toString(),
-                "Start Position" to startPositions[entry.lastValue(Sandstorm.startPosition)?.value ?: 0],
+                "Starting Position" to startPositions[entry.lastValue(Sandstorm.startPosition)?.value ?: 0],
+                "Starting Game Piece" to gamePieces[entry.dataPoints
+                        .firstOrNull { it.type == Sandstorm.gamePiece && it.time == 0 }?.value ?: 0],
                 "Hab Line" to (entry.lastValue(Sandstorm.habLine)?.value ?: 0),
                 "Camera Controlled" to entry.dataPoints.count { it.type == Sandstorm.cameraControl && it.value == 1 },
+                "Total Cargo" to 0,
+                "Total Hatch" to 0,
+                "SS Crossed Mid-line" to 0,
+                "SS Left Rocket Hatch" to 0,
+                "SS Left Rocket Cargo" to 0,
+                "SS Right Rocket Hatch" to 0,
+                "SS Right Rocket Cargo" to 0,
+                "SS Left Side Cargo" to 0,
+                "SS Left Side Hatch" to 0,
+                "SS Right Side Cargo" to 0,
+                "SS Right Side Hatch" to 0,
+                "SS Front Cargo" to 0,
+                "SS Front Hatch" to 0,
+                "Cargo Ship Cargo" to 0,
+                "Cargo Ship Hatch" to 0,
+                "Rocket 1 Cargo" to 0,
+                "Rocket 2 Cargo" to 0,
+                "Rocket 3 Cargo" to 0,
+                "Rocket 1 Hatch" to 0,
+                "Rocket 2 Hatch" to 0,
+                "Rocket 3 Hatch" to 0,
+                "Crossed Center Line" to 0,
+                "Total Defending Time" to 0,
+                "Defended Count" to 0,
+                "Total Defended Time" to 0,
                 "Climb Level" to climbLevels[entry.lastValue(Endgame.climbLevel)?.value ?: 0],
                 "Assisted Climb" to (entry.lastValue(Endgame.assistedClimb)?.value ?: 0),
                 "Lifting 1" to liftingLevels[entry.lastValue(Endgame.liftingRobot1)?.value ?: 0],
                 "Lifting 2" to liftingLevels[entry.lastValue(Endgame.liftingRobot2)?.value ?: 0],
                 "Start Time" to longFormatter.format(Date(entry.timestamp * 1000L)),
                 "Undo" to entry.undone,
+                "Outtake No Game Piece" to 0,
                 "Comments" to entry.comments
         ))
     }
